20cc0421ab38bc407ab5e02b859a6248187004da
Alan Vazquez <50334817+AlanIsaacV@users.noreply.github.com>
2025-09-19 13:02:40 -0600

    Feat (eza): Replace fish keybinding to use `eza`

--- modules/eza/dotfiles/config.fish
+++ modules/eza/dotfiles/config.fish
@@ -5,3 +5,5 @@ set IGNORED_PATTERNS "-I \"__pycache__|.git|node_modules|.idea\""
 
 set -gx EZA_CONFIG_DIR ~/.config/eza
 set -gx FZF_ALT_C_OPTS "--preview 'eza --tree -L2 --icons --group-directories-first --color=always $IGNORED_PATTERNS {}'"
+
+bind alt-l l
