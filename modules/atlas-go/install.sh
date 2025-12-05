3217e267020a00f9758a49a76e62fb8db2f225cb
Alan Vazquez <50334817+AlanIsaacV@users.noreply.github.com>
2025-12-05 09:33:08 -0600

    feat(atlas-go): add `atlas-go`

---
+++ modules/atlas-go/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Setting up atlas-go configuration..."
+
+# Packages are handled by DotCLI package manager integration
+# Custom commands will be executed by DotCLI
+
+echo "atlas-go setup completed!"
